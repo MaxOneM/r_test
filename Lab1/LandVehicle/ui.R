4bfc3988af06f9168ac608c6c342ab3c0012dfc0
MaxMatiash <blackjackmki@gmail.com>
2022-09-26 15:34:13 +0300

    DSSLab
    New commit

---
+++ Lab1/LandVehicle/ui.R
@@ -0,0 +1,53 @@
+library(shiny)
+
+#object for recognizing
+VectorObjectForRecognizing = c('Car', 'Motorcycle', 'Scooter', 'Bicycle')
+
+#characteristics
+VectorOfCharacteristicsNames = c('engine', 'no engine','two wheel', 'four wheel', 'weight > 1T', 'weight < 1T', '4 seats', '2 seats', '1 seat', 'ride standing up')
+VectorOfCharacteristicsValues = c('engine', 'noEngine', 'twoWheel', 'fourWheel', 'weightBig', 'weightSmall', '4seats', '2seats', '1seat', 'RideStandingUp')
+
+shinyUI(fluidPage(
+
+    navbarPage("Land vehicle",
+               
+      tabPanel('Study',
+        sidebarLayout(
+            sidebarPanel(
+              checkboxGroupInput(inputId = 'characterStd', 
+                                 'Entrer value:',
+                                 choiceNames = VectorOfCharacteristicsNames,
+                                 choiceValues = VectorOfCharacteristicsValues,
+              ),                    
+              submitButton('Apply')
+            ),
+    
+           
+            mainPanel(
+                textOutput("choiceCharacterStd"),
+                dataTableOutput('VehicleTableStd'),
+            )
+        )
+    ),
+    
+    
+    tabPanel('Recognition',
+             sidebarLayout(
+               sidebarPanel(
+                 checkboxGroupInput(inputId = 'characterRec', 
+                                    'Entrer value:',
+                                    choiceNames = VectorOfCharacteristicsNames,
+                                    choiceValues = VectorOfCharacteristicsValues
+                 ),                    
+                 submitButton('Apply')
+               ),
+               
+               
+               mainPanel(
+                 textOutput("choiceCharacterRec"),
+                 dataTableOutput('VehicleTableRec')
+               )
+             )
+      ),
+    )
+))
